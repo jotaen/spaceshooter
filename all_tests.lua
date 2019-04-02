4ff38339b57cb2fc3280da15dab6c098f9407db8
Damien Klinnert <mail@damienklinnert.de>
2019-04-02 19:00:33 +0200

    Move ui related tests to `ui` package

--- all_tests.lua
+++ all_tests.lua
@@ -1,6 +1,6 @@
 local lu = require('luaunit')
 
 require('src.vector_test')
-require('src.ship_test')
+require('src.ui.drawableShip_test')
 
 os.exit(lu.LuaUnit.run())
--- src/ui/drawableShip_test.lua
+++ src/ui/drawableShip_test.lua
@@ -1,10 +1,11 @@
 local assertVectorEquals = require('src.assertions').assertVectorEquals
 local ship = require('src.ship')
+local drawableShip = require('src.ui.drawableShip')
 local vector = require('src.vector')
 
 function test_unrotatedShip()
-    local s = ship.make(vector.make(0, 0), 0)
-    local triangle = ship.drawableShip(s)
+    local s = ship.Ship.make(vector.make(0, 0), 0)
+    local triangle = drawableShip.make(s)
 
     assertVectorEquals(triangle.v1, vector.make(1, 0))
     assertVectorEquals(triangle.v2, vector.make(-1, 0.5))
@@ -12,8 +13,8 @@ function test_unrotatedShip()
 end
 
 function test_rotated270Ship()
-    local s = ship.make(vector.make(0, 0), 270)
-    local triangle = ship.drawableShip(s)
+    local s = ship.Ship.make(vector.make(0, 0), 270)
+    local triangle = drawableShip.make(s)
 
     assertVectorEquals(triangle.v1, vector.make(0, -1))
     assertVectorEquals(triangle.v2, vector.make(0.5, 1))
